34a02acc975b32c669eb7066d94e9ffc11f7dd4d
Anhsapper <anhntse173567@fpt.edu.vn>
2025-02-12 16:49:44 +0700

    delete google auth provider

--- src/main.tsx
+++ src/main.tsx
@@ -11,13 +11,13 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <GoogleOAuthProvider clientId="256379527117-fif7v5tq0hu7d7c09nr4af1lb4e9nup7.apps.googleusercontent.com">
+
         <BrowserRouter>
           <StrictMode>
             <App />
           </StrictMode>
         </BrowserRouter>
-      </GoogleOAuthProvider>
+    
     </PersistGate>
   </Provider>
 );
